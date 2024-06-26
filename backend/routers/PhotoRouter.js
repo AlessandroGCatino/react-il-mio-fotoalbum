585a49e776c7f0a756dd83cb44abbb035d0e34d2
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-06-26 10:05:51 +0200

    secondo test

--- backend/routers/PhotoRouter.js
+++ backend/routers/PhotoRouter.js
@@ -2,7 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 
-router.get('/', index)
+router.get('/', index);
+
+router.post('/', validator(verifyRequest), create);
+
+router.use('/:id', validator(verifyID))
+
+// il check sull'id viene effettuato su tutte le route successive
+router.get('/:id', show);
+
+router.put('/:id', validator(verifyRequest), update);
+
+router.delete('/:id', destroy);
 
 
 
