5812af729e6926d39e81cdb0ef3860f9301fd452
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-06-26 12:41:27 +0200

    index photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -3,7 +3,49 @@ const e = require('express')
 const prisma = new PrismaClient();
 
 const index = async(req, res, next) =>{
+    try{
+        const { title, page=1, postPerPage=10 } = req.query;
+        let { published } = req.query 
 
+        if (published){
+            published === "true" ? published = true : published = false
+        }
+
+        // Paginazione
+        const offset = (page - 1) * postPerPage;
+        const totalPosts = await prisma.Post.count({ 
+            where: {
+                published,
+                title: {
+                    contains: title
+                }
+            }});
+        const totalPages = Math.ceil(totalPosts / postPerPage);
+        if (page > totalPages) {
+            throw new Error("La pagina richiesta non esiste.");
+        }
+
+        // Fine Paginazione
+        const posts = await prisma.Post.findMany({
+            where:{
+                published,
+                title: {
+                    contains: title
+                }
+            },
+            include: {
+                tags: true
+            },
+            orderBy: {
+                id: 'desc'
+            },
+            take: parseInt(postPerPage),
+            skip: offset
+        });
+        res.status(200).send({posts: posts, page: page, totalPages: totalPages, totalPosts: totalPosts});
+    } catch(e) {
+        next(e);
+    }
 }
 
 const create = async(req, res, next) =>{
