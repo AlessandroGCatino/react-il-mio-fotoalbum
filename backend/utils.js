1de2d19c8fdf03c513000d3da9afed51404b566a
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-06-27 14:45:25 +0200

    creata function per cancellare immagini

--- backend/utils.js
+++ backend/utils.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const fs = require('fs');
+const path = require('path');
 require("dotenv").config();
 
 const generateToken = (payload, expiresIn = '2h') => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
@@ -14,8 +16,19 @@ const comparePassword = async (password, hashPassword) => {
     return checkPassword;
 }
 
+const deletePic = (picFolder, filename) => {
+    try{
+        const filePath = path.join(__dirname, `/public/${picFolder}/` + filename);
+        fs.unlinkSync(filePath);
+    }
+    catch(err){
+        console.log(`Non sono riuscito ad eliminare la pic ${filename}.`)
+    }
+}
+
 module.exports = {
     hashPassword,
     comparePassword,
     generateToken,
+    deletePic
 }
